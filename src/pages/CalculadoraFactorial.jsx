3d4ee3da8e36d1b2d3362da6e7b9202a4be1ed69
Enrique Ansa <enriqueansa@gmail.com>
2023-07-24 01:22:51 -0400

    useLayoutEffect to Inicio and scrollTo(0,0) in Calculator

--- src/pages/CalculadoraFactorial.jsx
+++ src/pages/CalculadoraFactorial.jsx
@@ -6,7 +6,7 @@ const CalculadoraFactorial = () => {
   useEffect(()=>{
     window.scroll(0, 0)
   }, [])
-  s
+  
   return (
     <div className='row px-0 gap-0 mb-5 pb-5 h-100'>
       <h1 className='mt-4 mb-5'>Factorial</h1>
